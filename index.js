b5b38f6389375a43d9e4809f697d1e0c28e34ef5
Yasiru  Amarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-07-23 22:40:12 +0530

    Create index.js
    Signed-off-by: Yasiru  Amarasinghe  <93648091+YasiruAmarasinghe@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,40 @@
+const { WAConnection, MessageType } = require('@adiwajshing/baileys').default
+const makeWASocket = require("@adiwajshing/baileys").default
+const { exec, spawn, execSync } = require("child_process")
+const pino = require('pino')
+const fs = require('fs')
+const figlet = require('figlet')
+const qrcode = require("qrcode-terminal")
+const { delay, useSingleFileAuthState } = require("@adiwajshing/baileys")
+const { state, saveState } = useSingleFileAuthState(`./session.json`)
+function unauxmd() {
+  let sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: true,
+    browser: ["UNAUX-MD", "Opera", "30.0.0"]
+  })
+
+  sock.ev.on("connection.update", async (s) => {
+      console.log("Connecting......")
+      console.log(figlet.textSync('UNAUX -MD', {
+		font: 'Standard',
+		horizontalLayout: 'default',
+		vertivalLayout: 'default',
+		whitespaceBreak: false
+	}))
+    const { connection} = s
+    if (connection == "open") {
+      await delay(1000 * 10)
+      const session = fs.readFileSync('./session.json')
+      await sock.sendMessage(sock.user.id, { document: session, mimetype: 'application/json', fileName: `session.json`}) ;
+      process.exit(0)
+    }
+    if (
+      connection === "close"  ) {
+      unauxmd()
+    }
+  })
+  sock.ev.on('creds.update', saveState)
+  sock.ev.on('messages.upsert', () => { })
+}
+unauxmd()
