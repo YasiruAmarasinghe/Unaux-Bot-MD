774b578f84bdc6986abdb789bf19d7733dd74399
Yasiru  Amarasinghe <93648091+YasiruAmarasinghe@users.noreply.github.com>
2022-07-25 08:28:02 +0530

    Create config.js
    Signed-off-by: Yasiru  Amarasinghe  <93648091+YasiruAmarasinghe@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,32 @@
+let fs = require('fs')
+const toBool = (x) => x == 'true'
+const { Sequelize } = require('sequelize')
+const { existsSync } = require('fs')
+if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
+
+global.owner = OWNER: process.env.OWNER,
+global.alivelogo = 
+global.APIs = { 
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  hardianto: 'https://hardianto.xyz',
+}
+global.APIKeys = { 
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://hardianto.xyz': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+global.WM = STICKER_PACKNAME: process.env.STICKER_PACKNAME || 'Unaux',
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
